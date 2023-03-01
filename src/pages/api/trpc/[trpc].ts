8d357fdf63c91c15ecb9bdfc42606152f770c590
Jose Hernandez <josemanki@gmail.com>
2023-03-01 10:51:54 +0100

    Prod env

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -12,8 +12,14 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
       : undefined,
 });
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
